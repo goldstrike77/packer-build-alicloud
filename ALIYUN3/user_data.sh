e3648bab7fe9a4b329c833c3f4a83da331029ae6
suzhetao <suzhetao@gmail.com>
2026-02-20 11:11:40 +0800

    modified:   ALIYUN3/user_data.sh 	modified:   UBUNTU2404/user_data.sh

--- ALIYUN3/user_data.sh
+++ ALIYUN3/user_data.sh
@@ -3,3 +3,4 @@ useradd -G wheel -m -s /bin/bash ecs-admin
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/wheel
 chmod 750 /home/ecs-admin
 sed -i 's/wheel:x:10:ecs-admin/wheel:x:10:ecs-admin,root/' /etc/group
+echo "AllowUsers ecs-admin" >> /etc/ssh/sshd_config
